4b2167b7d96b2be0dd43bf1a94b7e27556946b6b
Deepak Sabhrawal <deepak.sabhrawal@gmail.com>
2020-04-11 08:02:05 +0530

    initial commit

---
+++ src/main/java/com/springbootdemo/SrpingbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingbootDemoApplication.class, args);
+	}
+
+}
